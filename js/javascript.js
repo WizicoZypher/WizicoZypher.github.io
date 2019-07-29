9ce79b6205103ccc8ef3371d733fbf12c1e97e1e
WizicoZypher <wizicozypher@outlook.com>
2019-07-29 22:06:56 +0800

    Navbar Refinement

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,8 @@
-var body = document.getElementById("fadetowhite");
+var whitescreen = document.getElementById("fadetowhite");
 function FadeOut() {
-    body.style.opacity="1";
+    whitescreen.style.opacity="1";
 }
 function FadeIn() {
-    body.style.opacity="0";
+    whitescreen.style.opacity="0";
 }
 onload(FadeIn());
